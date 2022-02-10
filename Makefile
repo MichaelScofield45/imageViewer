41cf979d789342c99a721de8e78b3593bfc1f796
MichaelScofield45 <paco.villamonte@gmail.com>
2022-02-10 14:57:54 -0600

    Render line and square
    Massive performance increase when compiling with -02 (make release).

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CXX = g++
 CXXFLAGS = -Wall -Wpedantic -std=c++20
-LDFLAGS = -lSDL2 -lm
+LDFLAGS = -lSDL2
 
 imageViewer: main.cpp
 	$(CXX) $(CXXFLAGS) main.cpp -o imageViewer $(LDFLAGS)
 
 debug: main.cpp
 	$(CXX) $(CXXFLAGS) -g main.cpp -o imageViewer $(LDFLAGS)
+
+release: main.cpp
+	$(CXX) $(CXXFLAGS) -O2 main.cpp -o imageViewer $(LDFLAGS)
 
 clean:
 	rm -rf imageViewer
--- main.cpp
+++ main.cpp
@@ -3,15 +3,11 @@
 #include <SDL2/SDL_mouse.h>
 #include <cstdint>
 #include <iostream>
-#include <memory>
 #include <string>
 #include <vector>
-#include <chrono>
-#include <thread>
-#include <cmath>
-#include <algorithm>
 
 constexpr uint32_t GREEN = 0xFF00FF00;
+constexpr uint32_t BLACK = 0x00000000;
 
 struct Window
 {
@@ -22,95 +18,112 @@ struct Window
 
 struct Coord
 {
-    uint32_t x;
-    uint32_t y;
+    int32_t x;
+    int32_t y;
 };
 
-const  static
-void error_message_memory(const char* msg)
+struct Rect
+{
+    Coord pos;
+    uint32_t width;
+    uint32_t height;
+};
+
+static inline void error_message_memory(const char* msg)
 {
     std::cerr << "[Error] Memory: Unable to allocate memory for "
-              << msg << " exiting..." << '\n';
+              << msg << "." << '\n';
 }
 
-constexpr size_t coord_to_index(Window &win, uint32_t x, uint32_t y)
+inline constexpr size_t coord_to_index(Window *win, uint32_t x, uint32_t y)
 {
-    return (win.w * y + x);
+    return (win->w * y + x);
 }
 
-constexpr Coord index_to_coord(Window &win, size_t index)
+inline constexpr Coord index_to_coord(Window *win, size_t index)
 {
-    return (Coord { (uint32_t)index % win.w, (uint32_t)index / win.w });
+    return (Coord { (int32_t)index % (int32_t)win->w, (int32_t)index / (int32_t)win->w });
 }
 
-constexpr float distance(Coord point, Coord pixel)
+constexpr float distance(Coord a, Coord b)
 {
     return sqrt(
-           ((float)point.x - (float)pixel.x) * ((float)point.x - (float)pixel.x) +
-           ((float)point.y - (float)pixel.y) * ((float)point.y - (float)pixel.y));
+           ((float)a.x - (float)b.x) * ((float)a.x - (float)b.x) +
+           ((float)a.y - (float)b.y) * ((float)a.y - (float)b.y));
 }
 
-constexpr float max_distance(Window *win, int mouse_x, int mouse_y)
+inline constexpr uint32_t val_to_grayscale(uint8_t x)
 {
-    if ((uint32_t)mouse_x <= (win->w / 2) && (uint32_t)mouse_y <= (win->h / 2)) { // Top left corner
-        return distance(Coord{(uint32_t)mouse_x, (uint32_t)mouse_y}, Coord{win->w, win->h});
-    } 
-    else if ((uint32_t)mouse_x > (win->w / 2) && (uint32_t)mouse_y < (win->h / 2)) { // Top right corner
-        return distance(Coord{(uint32_t)mouse_x, (uint32_t)mouse_y}, Coord{0, win->h});
-    } 
-    else if ((uint32_t)mouse_x > win->w / 2 && (uint32_t)mouse_y >= win->h / 2) { // Bottom right corner
-        return distance(Coord{(uint32_t)mouse_x, (uint32_t)mouse_y}, Coord{0, 0});
-    } 
-    else {                                                  // Bottom left corner
-        return distance(Coord{(uint32_t)mouse_x, (uint32_t)mouse_y}, Coord{win->w, 0});
+    return (0xFF000000 | (x << (8 * 2)) | (x << (8 * 1)) | (x << (8 * 0)));
+}
+
+inline constexpr float lerp(int32_t a, int32_t b, float t) {
+    return a + (b - a) * t;
+}
+  
+void draw_line(std::vector<uint32_t> *pix,
+               Window *win,
+               Coord a,
+               Coord b,
+               uint32_t col)
+{
+    uint32_t x_point = 0;
+    uint32_t y_point = 0;
+
+    for (float i = 0.f; i <= 1.f; i += 1.f / distance(a, b)) {
+        x_point = (uint32_t)lerp(a.x, b.x, i);
+        y_point = (uint32_t)lerp(a.y, b.y, i);
+        (*pix)[coord_to_index(win, x_point, y_point)] = col;
     }
-    return 0;
 }
 
-const uint32_t val_to_grayscale(uint32_t x)
-{
-    return (0xFF000000 +
-            (x << 16)  +
-            (x << 8)   +
-            (x));
-}
-
-const void draw_rectangle(std::vector<uint32_t> &pix,
-                          Window &win,
-                          uint32_t x,
-                          uint32_t y,
-                          uint32_t rect_width,
-                          uint32_t rect_height,
+void draw_rectangle(std::vector<uint32_t> *pix,
+                          Window *win,
+                          Rect *rect,
                           uint32_t col)
 {
-    uint32_t line_start = win.w * y + x;
+    if (rect->pos.x < 0) {
+        rect->pos.x = 0;
+        if (rect->pos.y < 0) 
+            rect->pos.y = 0;
+    } else if (rect->pos.y < 0) {
+        rect->pos.y = 0;
+        if (rect->pos.x < 0) 
+            rect->pos.x = 0;
+    }
 
-    for (size_t i = 0; i < rect_height; i++)
-    {
-        for (size_t j = 0; j < rect_width; j++)
-        {
-            pix[line_start + j] = col;
+    size_t line_start = win->w * rect->pos.y + rect->pos.x;
+
+    for (size_t i = 0; i < rect->height; i++) {
+        for (size_t j = 0; j < rect->width; j++) {
+            (*pix)[line_start + j] = col;
         }
-        line_start = win.w * (y + i) + x;
+        line_start = win->w * (rect->pos.y + i) + rect->pos.x;
     }
 }
 
-uint32_t parse_string(std::string str)
+constexpr bool mouse_in_rect(Rect *rect, int *mouse_x, int *mouse_y) {
+    Coord up_left_bound{rect->pos.x,
+                        rect->pos.y};
+    Coord down_right_bound{rect->pos.x + (int32_t)rect->width,
+                           rect->pos.y + (int32_t)rect->height};    
+
+    return (*mouse_x >= up_left_bound.x && *mouse_x <= down_right_bound.x &&
+            *mouse_y >= up_left_bound.y && *mouse_y <= down_right_bound.y);
+}
+
+const uint32_t parse_string(std::string str)
 {
     uint32_t acc = 0;
     uint32_t mult = 0;
-    for (int i = str.size()-1; i >= 0; i--)
-    {
+    for (int i = str.size()-1; i >= 0; i--) {
         mult = str[i] - 48;
-        if (mult < 0 || mult > 9)
-        {
+        if (mult < 0 || mult > 9) {
             std::cerr << "[Error] Arguments: Dimensions given contain non-numerical symbol." << '\n';
             exit(-1);
         }
-        if (mult > 0)
-        {
-            for (int j = str.size() - (i+1); j > 0; j--)
-            {
+        if (mult > 0) {
+            for (int j = str.size() - (i+1); j > 0; j--) {
                 mult *= 10;
             }
             acc += mult;
@@ -119,28 +132,27 @@ uint32_t parse_string(std::string str)
     return acc;
 }
 
-void clear_screen(std::vector<uint32_t> &screen_pixels, uint32_t color)
+inline void clear_screen(std::vector<uint32_t> *screen_pixels, uint32_t color)
 {
-    for (auto &p: screen_pixels) {
+    for (auto &p: *screen_pixels) {
         p = color;
     }
 }
 
 int main(const int argc, const char* argv[])
 {
-    if (argc < 3)
-    {
-        std::cerr << "[Error] Usage: Missing arguments for window_global dimensions." << '\n';
-        return -1;
+    if (argc < 3) {
+        std::cerr << "Usage: imageViewer [WINDOW_WIDTH] [WINDOW_HEIGHT]" << '\n';
+        return 1;
     }
+
     SDL_Event event;
     Window window_global;
     window_global.w = parse_string(argv[1]);
     window_global.h = parse_string(argv[2]);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cerr << "[Error] SDL2: trouble initializing the SDL2 library." << '\n';
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "[Error] Trouble initializing the SDL2 library." << '\n';
         return -1;
     }
 
@@ -165,39 +177,47 @@ int main(const int argc, const char* argv[])
     if (texture == nullptr) error_message_memory("texture");
 
     std::vector<uint32_t> pixels(window_global.w * window_global.h);
-    int x, y = 0; // Mouse position
-   
+    int x = 0, y = 0; // Mouse positions
+    Rect rect = { 20, 20, 50, 50 };
     bool running = true;
+    bool mouse_hold = false;
+
     while (running)
     {
         while(SDL_PollEvent(&event))
         {
-            if (event.type == SDL_QUIT)
-            {
-                running = false;
-                break;
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    mouse_hold = true;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    mouse_hold = false;
+                    break;
+                default:
+                    break;
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         SDL_PumpEvents();
         SDL_GetMouseState(&x, &y);
 
-        float max = max_distance(&window_global, x, y);
-        for (size_t i = 0; i < pixels.size(); i++) {
-            Coord pixel_coord = index_to_coord(window_global, i);
-            float calculated_distance = distance(Coord{(uint32_t)x, (uint32_t)y}, pixel_coord);
+        clear_screen(&pixels, BLACK);
+        draw_rectangle(&pixels, &window_global, &rect, val_to_grayscale(100));
+        draw_line(&pixels, &window_global, rect.pos, Coord{x, y}, GREEN);
 
-            pixels[i] = val_to_grayscale(floor(calculated_distance / max * 255));
+        if (mouse_hold && mouse_in_rect(&rect, &x, &y)) {
+            rect.pos.x = x - rect.width / 2;
+            rect.pos.y = y - rect.height / 2;
         }
-
 
         SDL_UpdateTexture(texture, nullptr, static_cast<const void*>(pixels.data()), window_global.w * sizeof(uint32_t));
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, nullptr, nullptr);
         SDL_RenderPresent(renderer);
     }
-
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window_global.win);
